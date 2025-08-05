5721ffab07e93320f7bf67ded549ac341d9ab256
Adriano Leite da Silva <adrianoleitedasilva@outlook.com>
2025-08-05 19:01:00 -0300

    update: exportando o model de perguntas

--- 04_Projeto01/database/Pergunta.js
+++ 04_Projeto01/database/Pergunta.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const connection = require("./database");
 
-const Pergunta = connection.define("pergunta", {
+const Pergunta = connection.define("perguntas", {
   titulo: {
     type: Sequelize.STRING,
     allowNull: false,
@@ -13,3 +13,5 @@ const Pergunta = connection.define("pergunta", {
 });
 
 Pergunta.sync({ force: false }).then(() => {}); // force: false não vai criar a tabela, caso ela já exista
+
+module.exports = Pergunta;
